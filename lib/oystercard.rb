ad47a1e4cc2f04696a83f09fe15b547f38e1039a
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-07-11 15:08:48 +0000

    add the balance + MAXIMUM_BALANCE

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,16 @@
+
+
 class Oystercard 
     DEFAULT_BALANCE = 0
+    MAXIMUM_BALANCE = 90
     attr_reader :balance
     
     def initialize(balance = DEFAULT_BALANCE)
         @balance = balance
     end
-    def top_up(sum)
-        @balance += sum
+    def top_up(amount)
+        fail "card limitted to £90" if (@balance+amount)>MAXIMUM_BALANCE
+        @balance += amount
         @balance
     end
         
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,11 +7,19 @@ describe Oystercard do
     end
 describe '#top_up' do
    it 'tells you when you top up' do 
-       expect(subject.top_up(1)).to eq 1
+       #expect(subject.top_up(1)).to eq 1 ======= Our code
+       expect{subject.top_up(1)}.to change{subject.balance}.by(1)
       end
    end
- 
-    
+   
+   it 'tells you there is a £90 limit' do
+       maximum_balance = Oystercard::MAXIMUM_BALANCE
+       card = Oystercard.new(maximum_balance)
+       expect{card.top_up(maximum_balance)}.to raise_error "card limitted to £#{maximum_balance}"
+   end
+
+
+
 end
 
 
