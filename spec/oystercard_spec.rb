a6dd979a23a1e83fd3d3a5c3891ee67df47c11aa
Georgia Mills <georgee_123@hotmail.com>
2016-07-11 16:12:27 +0100

    added balance of 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,14 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  #subject(:card) {Oystercard.new (:money)}
+
+  it 'has a balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
+
+  #it 'tops up card with money' do
+  #  expect{card.top_up(money)}.to eq money
+#  end
+
 end
