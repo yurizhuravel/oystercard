d794819bf7fec4979be2af60a05f62f0febafa34
yurizhuravel <zhuravel@gmail.com>
2016-07-12 09:48:45 +0100

    Day two branch created

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,14 @@
 class Oystercard
 
-attr_reader :balance
+  attr_reader :balance
 
-def initialize
-  @balance = 0
-end
+  def initialize
+    @balance = 0
+  end
 
-def top_up(amount)
-  @balance += amount
-end
+  def top_up(amount)
+    @balance += amount
+  end
 
 end
+#
