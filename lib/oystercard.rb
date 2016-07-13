d9f3ed81b4c6968d702564030f5cbe98b37c6b04
yurizhuravel <zhuravel@gmail.com>
2016-07-13 10:46:54 +0100

    Adds touch in and out functionality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,19 +1,38 @@
 
 
-class Oystercard 
-    DEFAULT_BALANCE = 0
-    MAXIMUM_BALANCE = 90
-    attr_reader :balance
-    
-    def initialize(balance = DEFAULT_BALANCE)
-        @balance = balance
-    end
-    def top_up(amount)
-        fail "card limitted to £90" if (@balance+amount)>MAXIMUM_BALANCE
-        @balance += amount
-    end
-    
-    def deduct(amount)
-        @balance -= amount
-    end    
+class Oystercard
+  DEFAULT_BALANCE = 0
+  MAXIMUM_BALANCE = 90
+  attr_reader :balance, :in_journey
+
+  def initialize(balance = DEFAULT_BALANCE)
+      @balance = balance
+      @in_journey = false
+  end
+
+  def top_up(amount)
+      fail "card limitted to £90" if limit_reached(amount)
+      @balance += amount
+  end
+
+  def limit_reached(amount)
+    @balance + amount > MAXIMUM_BALANCE
+  end
+
+  def deduct(amount)
+      @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,36 +1,53 @@
 require 'oystercard'
 
 describe Oystercard do
-    
-    subject(:card) {Oystercard.new }
-    
-    
+
+subject(:card) {Oystercard.new }
+
     it 'tells your balance is 0' do
-        expect(card.balance).to eq 0
+      expect(card.balance).to eq 0
     end
-describe '#top_up' do
-   it 'tells you when you top up' do 
-       #expect(subject.top_up(1)).to eq 1 ======= Our code
-       expect{card.top_up(1)}.to change{card.balance}.by(1)
+
+    describe '#top_up' do
+      it 'tells you when you top up' do
+        expect{card.top_up(1)}.to change{card.balance}.by(1)
       end
-   end
-   
-   it 'there is a £90 limit' do
-       maximum_balance = Oystercard::MAXIMUM_BALANCE
-       card = Oystercard.new(maximum_balance)
-       expect{card.top_up(maximum_balance)}.to raise_error "card limitted to £#{maximum_balance}"
-   end
-   
-   describe '#deduct' do
-       it 'deducts the fee' do
-           expect{card.deduct(1)}.to change{card.balance}.by(-1)
-      end
-   
-#   it 'Can\'t deduct from £0' do
- 
    end
 
+   it 'there is a £90 limit' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      card = Oystercard.new(maximum_balance)
+      expect{card.top_up(maximum_balance)}.to raise_error "card limitted to £#{maximum_balance}"
+   end
+
+   it 'initially is not in journey' do
+     expect(card).not_to be_in_journey
+  end
+
+   describe '#deduct' do
+      it 'deducts the fee' do
+        expect{card.deduct(1)}.to change{card.balance}.by(-1)
+      end
+   end
+
+  describe '#touch_in and #touch_out' do
+
+    before(:each)do
+      card.touch_in
+    end
+
+     describe '#touch_in' do
+       it 'touches the card in' do
+         expect(card).to be_in_journey
+       end
+     end
+
+     describe '#touch_out' do
+       it 'touches the card out' do
+         card.touch_out
+         expect(card).not_to be_in_journey
+       end
+     end
+ end
 
 end
-
-
